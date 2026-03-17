2625ce3439c0fad86e3a186474580c22424bbfd8
larrieTau <kasibantelarry@gmail.com>
2026-03-17 16:52:54 +0300

    Add AdminPlacementManagement screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import WeeklyLogbook from './pages/student/weeklylogbook'
 import AdminEvaluations from './pages/admin/adminEvaluations'
 import AdminReports from './pages/admin/Adminreports'
 import AdminUsers from './pages/admin/AdminUsers'
+import AdminPlacementManagement from './pages/admin/AdminPlacementManagement'
 import './App.css'
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
         <Route path="/admin/evaluations" element={<AdminEvaluations />} />
         <Route path="/admin/reports" element={<AdminReports />} />
         <Route path="/admin/users" element={<AdminUsers />} />
+        <Route path="/admin/placements" element={<AdminPlacementManagement />} />
       </Routes>
     </Router>
   )
