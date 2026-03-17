11a8d8bded9ad02f7d6f3c6a9cf9a560492faa53
blaisedavis21 <blaisedavis207@gmail.com>
2026-03-17 15:23:15 +0300

    updated supervisor model

--- backend/api/models.py
+++ backend/api/models.py
@@ -91,5 +91,13 @@ class SupervisorReview(models.Model):
         on_delete=models.CASCADE,
         related_name='review'
     )
-    
+    supervisor = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        limit_choices_to={'role': 'workplace_supervisor'}
+    )
+    comment = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='reviewed')
+    reviewed_at = models.DateTimeField(auto_now_add=True)
 
