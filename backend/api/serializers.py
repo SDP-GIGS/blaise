66933c3cfcdd623ce6ea44bd63fe42eac02afd0a
blaisedavis21 <blaisedavis207@gmail.com>
2026-03-28 13:30:35 +0300

    serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -59,3 +59,8 @@ class EvaluationSerializer(serializers.ModelSerializer):
         fields = ['id', 'student', 'student_name', 'evaluator', 'evaluator_name', 'score', 'comments', 'evaluation_type', 'date']
         read_only_fields = ['date']
 
+
+
+
+
+
