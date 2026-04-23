e5d541e89a33e2da43839f729a2aebf8b72f27d7
blaisedavis21 <blaisedavis207@gmail.com>
2026-04-23 15:30:44 +0300

    add allowed origins for CORS in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -143,6 +143,8 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://localhost:3000',
+    "https://iles-platform.netlify.app",
+    "http://127.0.0.1:5173",
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
