4f5222c39d1cb1ad3ff09fba3d2743f06bea9027
blaisedavis21 <blaisedavis207@gmail.com>
2026-05-05 10:40:32 +0300

    add CORS_ALLOWED_ORIGINS for production domain

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,11 +140,12 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://localhost:3000',
-    "https://iles-platform.netlify.app",
-    "http://127.0.0.1:5173",
-]
+        'http://localhost:5173',
+        'http://localhost:3000',
+        "https://iles-platform.netlify.app",
+        "http://127.0.0.1:5173",
+        "https://blaise.tagooledavid.com",
+        ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://iles-platform.netlify.app",
