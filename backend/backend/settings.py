f997dadc15bfe5e3b2a16a000018b1d428b92db3
blaisedavis21 <blaisedavis207@gmail.com>
2026-06-02 22:24:18 +0300

    Add database configuration using dj-database-url in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,9 @@
 from datetime import timedelta
 from pathlib import Path
+import dj_database_url
+import os
+
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -52,10 +56,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
